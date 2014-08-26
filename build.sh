7a7fc6e78c4e79ca3a4aa01cd7e3528b649b27ba
Jongse <jspark@gatech.edu>
2014-08-26 14:41:46 -0400

    build script added

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd jsr308-langtools/make ; ls
+cd ../../annotation-tools/asmx ; ls
+cd ../ ; ls
+cd ../checker-framework/checkers/ ; ls
+cd ../../checker-runtime/ ; ls
+cd ../r2.enerj/ ; ls
+cd ../chord/main ; ls
+cd ../../r2.analysis/ ; ls
+cd ../r2.optimization/ ; ls
