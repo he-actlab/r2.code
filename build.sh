5a1aefab6ae0f44218a9be95477a43dd28272131
Jongse <jspark@gatech.edu>
2014-08-26 14:43:49 -0400

    minor

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd jsr308-langtools/make ; ls
-cd ../../annotation-tools/asmx ; ls
-cd ../ ; ls
-cd ../checker-framework/checkers/ ; ls
-cd ../../checker-runtime/ ; ls
-cd ../r2.enerj/ ; ls
-cd ../chord/main ; ls
-cd ../../r2.analysis/ ; ls
-cd ../r2.optimization/ ; ls
+cd jsr308-langtools/make ; ant
+cd ../../annotation-tools/asmx ; ant
+cd ../ ; ant
+cd ../checker-framework/checkers/ ; ant
+cd ../../checker-runtime/ ; ant
+cd ../r2.enerj/ ; ant
+cd ../chord/main ; ant
+cd ../../r2.analysis/ ; ant
+cd ../r2.optimization/ ; ant
