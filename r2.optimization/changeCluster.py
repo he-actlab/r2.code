3c5cc506bd4aab47ac585b609468ec3dbe768f2c
Jongse <jspark@gatech.edu>
2014-08-26 17:37:05 -0400

    make a tool to switch the sytem-wide python to local one

--- r2.optimization/changeCluster.py
+++
@@ -1,8 +0,0 @@
-#!/home/jspark/projects/expax/python-2.7/python
-
-import os, sys
-
-os.system("sed -i 's/\/usr\/bin\/python/\/home\/jspark\/projects\/expax\/python-2.7\/python/g' *.py")
-os.system("sed -i 's/\/usr\/bin\/python/\/home\/jspark\/projects\/expax\/python-2.7\/python/g' ../apps/*.py")
-os.system("sed -i 's/\/usr\/bin\/python/\/home\/jspark\/projects\/expax\/python-2.7\/python/g' ../apps/*.py.template")
-os.system("sed -i 's/\/usr\/local\/bin\/python2.7/\/home\/jspark\/projects\/expax\/python-2.7\/python/g' ../apps/*.py")
---
+++ switchToLocalPython.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os, sys
+
+directories = ['r2.optimization', 'r2.apps']
+
+newPythonPath = os.environ['RESEARCH'] + '/r2.code/Python.2.7.5/python' 
+for ch in newPythonPath:
+	if ch == '/':
+		_newPythonPath += '\/'
+	else:
+		_newPythonPath += ch
+print _newPythonPath
+
+for directory in directories:
+	print("sed -i 's/\/usr\/bin\/python/" + _newPythonPath  + "/g' ./" + directory  +  "/*.py")
+	print("sed -i 's/\/usr\/bin\/python/" + _newPythonPath  + "/g' ./" + directory  +  "/*.py.template")
+	print("sed -i 's/\/usr\/local\/bin\/python2.7/" + _newPythonPath  + "/g' ./" + directory  +  "/*.py.template")
+	os.system("sed -i 's/\/usr\/bin\/python/" + _newPythonPath  + "/g' ./" + directory  +  "/*.py")
+	os.system("sed -i 's/\/usr\/bin\/python/" + _newPythonPath  + "/g' ./" + directory  +  "/*.py.template")
+	os.system("sed -i 's/\/usr\/local\/bin\/python2.7/" + _newPythonPath  + "/g' ./" + directory  +  "/*.py.template")
